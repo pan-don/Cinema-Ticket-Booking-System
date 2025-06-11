4d45051689e32bb8c674fc8b416955da64a4f927
Syalaisha-Nisrina <ichanisrina04@gmail.com>
2025-06-11 08:00:05 +0700

    userperson

--- Cinema-Management-System/src/main/java/com/project/mvc/model/Person.java
+++ Cinema-Management-System/src/main/java/com/project/mvc/model/Person.java
@@ -1,15 +1,13 @@
 package com.project.mvc.model;
 
-import  jakarta.persistence.Column;
+import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.*;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 
 @MappedSuperclass
 @Getter
